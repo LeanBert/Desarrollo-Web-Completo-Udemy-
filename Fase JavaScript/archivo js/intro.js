543d64807ee1d6e55f5d88a7b7ba8125d84aa909
LeanBert <leanbertero@hotmail.com>
2023-04-14 13:16:41 -0300

    agregue archivos js + html (para ejercicio de js)~

--- Fase JavaScript/archivo js/intro.js
+++ Fase JavaScript/archivo js/intro.js
@@ -1,3 +1,13 @@
 // variables
 
 var producto = 'Audifonos Gamer';
+
+let producto = "micro";
+
+function dividir(a, b) {
+
+    if (b === 0) {
+        throw new Error("&quot;No se puede dividir entre cero&quot");
+    }
+    return a / b;
+}
