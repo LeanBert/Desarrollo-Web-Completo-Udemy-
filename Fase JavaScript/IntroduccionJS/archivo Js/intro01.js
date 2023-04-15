fde931ef96c08cd05960479138a060d8102ba89d
LeanBert <leanbertero@hotmail.com>
2023-04-14 23:02:41 -0300

    aprendiendo js

---
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro01.js
@@ -0,0 +1,22 @@
+// alert("Bienvenido a mi web")
+//variables
+
+var producto = 'Audifonos Gamer'; //iniciar variable y asignarle valor
+
+var disponible; //iniciamos la variable sin valor
+
+producto = true; //reasignando el valor de la variable
+
+//crea tres variables en una sola, para evitar repetir (--var--)
+var producto1 = 'computadora';
+disponible = true;
+categoria = 'computadoras';
+
+
+//las variables no pueden iniciar con numeros, pero si ese numero puede estar al final.
+
+// var 1disponible;
+
+
+
+
