3e921b49a1e6c9298a047e269a52f394ef63989f
LeanBert <leanbertero@hotmail.com>
2023-04-15 00:37:02 -0300

    conociendo js a fondo

---
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro03.js
@@ -0,0 +1,16 @@
+
+// string o cadenas de texto
+const producto = "Monitor de 20 pulgadas";
+
+console.log(producto);
+
+
+
+//typeoff = que tipo de dato tiene una variable
+
+
+
+
+// console.log(typeof producto);
+
+
