1e7fb894a8a17db1cff4470355e7dceaaa0960a1
LeanBert <leanbertero@hotmail.com>
2023-04-15 00:02:58 -0300

    trabajando en js sobre el archivo intro03.js

--- Fase JavaScript/IntroduccionJS/archivo Js/intro01.js
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro01.js
@@ -5,7 +5,7 @@ var producto = 'Audifonos Gamer'; //iniciar variable y asignarle valor
 
 var disponible; //iniciamos la variable sin valor
 
-producto = true; //reasignando el valor de la variable
+//producto = true; //reasignando el valor de la variable
 
 //crea tres variables en una sola, para evitar repetir (--var--)
 var producto1 = 'computadora';
@@ -16,6 +16,22 @@ categoria = 'computadoras';
 //las variables no pueden iniciar con numeros, pero si ese numero puede estar al final.
 
 // var 1disponible;
+
+var disponible_ = true;
+
+var nombre_producto = 'Monitor HD'; //underscore
+
+var nombreProducto = 'Monitor HD'; // camelcase
+
+var NombreProducto = 'Monitor HD'; //PascalCase
+
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
+
+//A DIFERENCIA DE LAS VARIABLLES LET NO ES NECESARIO QUE INICIALICEN CON UN VALOR.
+
+//las variables const siempre deben iniciar con un valor (SIEMPRE)
 
 
 
---
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro02.js
@@ -0,0 +1,16 @@
+let producto = 'Audifonos Gamer';
+
+let disponible;
+
+disponible = true;
+
+let producto1 = 'computadora';
+disponible = true;
+categoria = 'Computadoras';
+
+let disponible_;
+
+let nombre_producto = 'Monitor HD';
+let nombreProducto = 'Monitor HD';
+let NombreProducto = 'Monitor HD';
+let nombreproducto = 'Monitor HD';
