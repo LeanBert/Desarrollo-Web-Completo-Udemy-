f0159e608db9e4d3887c623dc1b32d17ac9500e5
LeanBert <leanbertero@hotmail.com>
2023-04-19 14:26:13 -0300

    nuevo archivo js - controlar el orden de las operaciones

--- Fase JavaScript/IntroduccionJS/archivo Js/intro05.js
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro05.js
@@ -1,1 +1,12 @@
-alert('Bienvendo =)');
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.7);
+resultado = Math.ceil(2.7); //siempre redondea hacia arriba
+resultado = Math.floor(2.9); //siempre redondea hacia abajo
+
+
+
+console.log(resultado);
+
+
