9a4ca1957a2b6779b26c29be24aef34ff3508fb3
LeanBert <leanbertero@hotmail.com>
2023-04-19 02:03:14 -0300

    aprendiendo el objeto Maths en Js

---
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro04.js
@@ -0,0 +1,30 @@
+//numeros     
+
+
+/*creacion de dos variables con dos valores diferentes */ 
+
+//numeros con variables constantes
+const nombre = 'Leandro';
+const numero1 = 100;
+const numero2 = 200;
+// const numero3 = 200.20;
+
+const cliente1 = 'pedro';
+const cliente2 = 'lean';
+
+//muestra en pantalla los valores
+console.log(numero1);
+console.log(numero2);
+console.log(nombre);
+// console.log(numero3);
+
+
+//operaciones con js (tambien muestra en pantalla los resultados de los valores de cada una de las variables)
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+console.log('Primer cliente' + ' = ' + cliente1 + ' - ', ' Segundo cliente' + ' = ' + cliente2);
+
