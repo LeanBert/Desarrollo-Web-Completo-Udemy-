93b58fbda51ab9185e96be401e8d5095e70efa86
LeanBert <leanbertero@hotmail.com>
2023-04-19 01:09:04 -0300

    agregando archivos js

--- Fase JavaScript/IntroduccionJS/archivo Js/intro03.js
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro03.js
@@ -1,16 +1,29 @@
 
 // string o cadenas de texto
-const producto = "Monitor de 20 pulgadas";
+//const (tipo de dato constante)
+const producto = "APRENDIENDO JAVA SCRIPT DESARROLLO WEB COMPLETO";
+const producto2 = "patineta";
+const tweet = 'hola, estoy practicando JavaScript ';
+const email = 'correo@correo.com'
 
-console.log(producto);
-
-
+/*.lenght --» dice la catidad de caracteres que tiene una cadena de texto*/
+console.log(tweet.length); 
+console.log(producto2);
+console.log(tweet.indexOf('practicando'));
 
 //typeoff = que tipo de dato tiene una variable
-
-
-
-
 // console.log(typeof producto);
+//indexOff -(retorna la posición) sirve para bscar una determinada palabra dentro de una texto
+
+
+//include(retorna true or false)
+console.log(tweet.includes('estoy'))
+console.log(tweet.includes('hello'))
+console.log(tweet.indexOf('@'))
+
+
+
+
+
 
 
