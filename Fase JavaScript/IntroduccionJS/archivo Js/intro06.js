37d12424a11ae741cc144c8a8950ad3457f26d67
LeanBert <leanbertero@hotmail.com>
2023-04-19 23:47:32 -0300

    trabajando en js (boleans)

---
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro06.js
@@ -0,0 +1,14 @@
+//controlar el orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) *2;
+
+// console.log(resultado);
+
+resultado = (100 + 200 + 300) *1.16; 
+
+let puntaje = 10;
+// ++puntaje;
+puntaje++;
+console.log(puntaje);
---
+++ Fase JavaScript/IntroduccionJS/archivo Js/intro07.js
@@ -0,0 +1,9 @@
+//Concatenación template strings
+
+const nombre = 'Leandro'
+const email = 'correo@correo.com'
+
+console.log('Nombre de cliente' + ' - ' + nombre + ' - ' + 'E-mail ' + email);
+// console.log(mail);
+
+console.log(); //este va con comillas inclinadas que no se donde estarán
